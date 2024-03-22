79489a2d8448a6f34642a0792130ba6f3db33b4b
Seunghyun Kim <seunghyunkim@Seunghyunui-MacBookPro.local>
2024-03-23 00:45:09 +0900

    ts -> js 변경

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import { Button } from "primereact/button";
+
+function App() {
+  return (
+    <>
+      <Button label="Check" icon="pi pi-check" />
+    </>
+  );
+}
+
+export default App;
